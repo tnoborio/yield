f51d3e6813296524af6fc2c4fbdf8cf2ebe11426
Tokusei Noborio <tokusei@nyampass.com>
2026-02-23 21:32:00 +0900

    return handler var for live coding

--- src/yield/handler.clj
+++ src/yield/handler.clj
@@ -1,8 +1,10 @@
 (ns yield.handler
   (:require [integrant.core :as ig]))
 
+(defn index [_request]
+  {:status 200
+   :headers {"content-type" "application/json"}
+   :body "{\"message\":\"okaa\"}"})
+
 (defmethod ig/init-key :yield.handler/index [_ _]
-  (fn [_request]
-    {:status 200
-     :headers {"content-type" "application/json"}
-     :body "{\"message\":\"ok\"}"}))
+  #'index)
